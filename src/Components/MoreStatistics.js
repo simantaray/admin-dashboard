a8791ee3579c2f6a9524fe855693e611a934122f
simantaray <simantaray89@gmail.com>
2022-04-27 21:53:14 +0530

    add : schedules

---
+++ src/Components/MoreStatistics.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+export default function MoreStatistics() {
+    const classes=[
+        {id:1,title:"English", startingTime:"09am" ,endingTime:"09:45am"},
+        {id:2,title:"Maths", startingTime:"09am" ,endingTime:"09:45am"},
+        {id:3,title:"Science", startingTime:"09am" ,endingTime:"09:45am"},
+        {id:4,title:"Psychology", startingTime:"09am" ,endingTime:"09:45am"},
+    ]
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-4 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 md:w-1/2">
+              <div className="flex rounded-lg h-full bg-white p-4 flex-col">
+                <div className="flex items-center mb-3">
+                  <h2 className="text-gray-900 font-semibold text-lg title-font ">
+                    Today's Schedule
+                  </h2>
+                </div>
+                <div className="flex flex-col w-[100%] xl:p-4 md:pt-2 justify-between">
+                 {classes.map((data)=>(
+                         <div key={data.id} className='flex flex-raw justify-between shadow-md p-1 m-1 text-center rounded-md'>
+                             <div className='flex  justify-between  p-1 '>
+                                <h1 className='text-xl rounded-full p-1 text-red-600 h-10 w-10 bg-red-100'>{data.id}</h1>
+                                <h1 className='p-1.5 font-semibold xl:pl-6'>{data.startingTime} - {data.endingTime}</h1>
+                                <h1 className='p-1.5 font-semibold xl:pl-8'>{data.title}</h1>
+                             </div>
+                             
+                             <button type="button" class=" hover:text-white border-2 border-blue-700 hover:bg-blue-800  font-semibold rounded-lg text-md px-8 py-2 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Join Class</button>
+                         </div>
+                     ))}
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/2">
+              <div className="flex rounded-lg h-full bg-white p-4 flex-col">
+                <div className="flex items-center mb-3">
+                  <h2 className="text-gray-900 text-lg title-font font-semibold">
+                    Monthly Performance
+                  </h2>
+                </div>
+                <div className="flex w-[100%] xl:p-4 md:pt-2 justify-between">
+                 asdfghjn
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
